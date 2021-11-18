9862b0b65a5b333911d1f904d94b67fc4c7f1a19
Nguyễn Trung Dũng - CMC Global DU21 <ntdung7@cmcglobal.vn>
2021-11-18 17:18:01 +0700

    Update career

--- src/components/Career/timelineElement.js
+++ src/components/Career/timelineElement.js
@@ -2,6 +2,16 @@ let timelineElements = [
     {
         id: 1,
         title: "Backend Developer",
+        location: "CMC Global, 80 Dich Vong Hau Street",
+        description: "Working as backend developer role in project.",
+        buttonText: "View my company",
+        date: "September 2020 - now",
+        icon: "work",
+        link: "https://www.cmc.com.vn/cmc-global/"
+    },
+    {
+        id: 2,
+        title: "Backend Developer",
         location: "VMO Group, 18 Ton That Thuyet Street.",
         description: "Working as backend developer role in project.",
         buttonText: "View my company",
@@ -10,7 +20,7 @@ let timelineElements = [
         link: "https://www.vmo.group/"
     },
     {
-        id: 2,
+        id: 3,
         title: "Internship",
         location: "University of Ostrava, Fráni Šrámka 1121/3, 709 00 Ostrava-Mariánské Hory a Hulváky, Czechia.",
         description: "Do an internship on the topic of Anomaly detection system based in image clustering and classification(apply Machine Learning with Python) at Ostrava University under the guidance of Professor Sebastián Basterrech. Mark: 15/20 (French scale) ",
@@ -20,7 +30,7 @@ let timelineElements = [
         link: "https://sites.google.com/view/sbasterrech/home"
     },
     {
-        id: 3,
+        id: 4,
         title: "Study",
         location: "University of Science and Technology of Ha Noi, 18C Hoang Quoc Viet Street.",
         description: "Study Bachelor of Information Technology and Communication.Mark: 13.24/20 (French scale) equal 3/4 (Viet Nam scale).",
@@ -30,5 +40,6 @@ let timelineElements = [
         link: "https://www.usth.edu.vn/en/"
 
     }
+
 ]
 export default timelineElements
